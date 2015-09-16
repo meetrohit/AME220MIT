bc4da6af8f5df323ff17b6a98d49e34048abce60
Mit <Mit@epifinder.com>
2015-09-16 11:19:04 -0700

    class file added

---
+++ test.js
@@ -0,0 +1,21 @@
+var boxStatus=[];
+ for (var i =0; i<=30; i++)
+     {
+         boxStatus[i] = "left";
+     }
+
+
+var buttonClicked= function(n, easing)
+{
+    var id ="#button" + n.toString();
+    var pageWidth = $("body").width();
+    var boxWidth = 150;
+    if (boxStatus[n] == "left"){
+        $(id).animate({"margin-left": pageWidth - boxWidth + "px","color": "red"}, 1000, easing)
+        boxStatus[n] = "right";
+    }
+    else{   
+        $(id).animate({"margin-left": "0px","color": "black"}, 1000, easing)
+        boxStatus[n] = "left";
+    }
+}
